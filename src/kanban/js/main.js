370293895d86ddda0e2b37ed2c6b8b60e1c08965
Ardent24 <wrb.re-cords@yandex.ru>
2020-11-11 00:21:07 +0700

    kanban-dropdown: edit destroy

--- src/kanban/js/main.js
+++ src/kanban/js/main.js
@@ -1,43 +1,43 @@
 const btnAvatar = document.querySelector('.header-avatar__js');
 const arrowAvatar = btnAvatar.querySelector('.header-avatar__icon');
 const boxDropdown = document.querySelector('.header-users');
+const dropdown = document.createElement('ul');
 const controlDropdown = funcDropdown();
 
 function funcDropdown() {
-  const dropdown = document.createElement('ul');
   dropdown.classList.add('header-dropdown');
-  return {
-    create: () => {
-      dropdown.insertAdjacentHTML('afterbegin', `
+  dropdown.insertAdjacentHTML('afterbegin', `
   <li class="header-dropdown__item"><a class="header-dropdown__link" href="#">My account</a></li>
   <li class="header-dropdown__item"><a class="header-dropdown__link" href="#">My tasks</a></li>
   <li class="header-dropdown__item"><a class="header-dropdown__link" href="#">Log out</a></li>`);
+  return {
+    create: () => {
       boxDropdown.appendChild(dropdown);
-
-      document.addEventListener('click', function destroyOutSide (event) {
-        if (!dropdown.contains(event.target) && !btnAvatar.contains(event.target)) {
-          arrowAvatar.classList.remove('active');
-          controlDropdown.destroy();
-        }
-      });
     },
     destroy: () => {
       dropdown.classList.add('hide');
-      setTimeout(()=>{
+      setTimeout(() => {
         dropdown.classList.remove('hide')
-        dropdown.innerHTML = "";
+        dropdown.remove();
       }, 500)
     }
   }
 };
 
 btnAvatar.addEventListener('click', () => {
-  if (arrowAvatar.classList.contains('active')){
+  if (arrowAvatar.classList.contains('active')) {
     arrowAvatar.classList.remove('active');
     controlDropdown.destroy();
-  } else{
+  } else {
     arrowAvatar.classList.add('active');
     controlDropdown.create();
+  }
+});
+
+document.addEventListener('click', (event) => {
+  if (!dropdown.contains(event.target) && !btnAvatar.contains(event.target)) {
+    arrowAvatar.classList.remove('active');
+    controlDropdown.destroy();
   }
 });
 
